d4a234312f138794fdca66e4f77c4f69e88e5984
Alex Necakov <alexnecakov@gmail.com>
2017-09-18 07:49:57 -0400

    Addded Sample Classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMecanum.java
@@ -79,6 +79,7 @@ public class TeleOpMecanum extends LinearOpMode {
     double rotate;
 
     //dank comment <code></code>
+    //stank comment code
     final static int ENCODER_CPR = 1120;
     final static double WHEEL_CIRC = 4 * Math.PI;
     // 1 tile length is 24 inches
