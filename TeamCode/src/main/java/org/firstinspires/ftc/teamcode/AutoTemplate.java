a3d25c5fa5450854f6fcf7d9e3240fa1706dc11e
Alex Necakov <alexnecakov@gmail.com>
2017-10-06 18:15:19 -0400

    Created empty auto template

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTemplate.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+/**
+ * Created by Dragonoids on 2/27/2017.
+ */
+@Disabled
+@Autonomous(name="Auto Template", group="Blue")
+public class AutoTemplate extends DragonoidsAuto{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+
+        while (opModeIsActive()) {
+
+            break;
+        }
+    }
+}
