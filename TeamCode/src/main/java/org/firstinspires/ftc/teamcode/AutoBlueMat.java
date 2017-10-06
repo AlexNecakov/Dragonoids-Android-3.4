89fa3d21fe2605c9db73a803ab2c6e37913bca23
Alex Necakov <alexnecakov@gmail.com>
2017-10-06 18:15:51 -0400

    Added auto mat blue, start next to audience

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueMat.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Created by Dragonoids on 2/27/2017.
+ */
+@Autonomous(name="Blue Auto Mat", group="Blue")
+public class AutoBlueMat extends DragonoidsAuto{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+
+        while (opModeIsActive()) {
+            break;
+        }
+    }
+}
+
