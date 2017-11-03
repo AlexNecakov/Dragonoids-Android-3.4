f1661eddb2581fc478bc2e5fa407b93fc98c51fb
Alex Necakov <alexnecakov@gmail.com>
2017-11-03 17:05:50 -0400

    Adjusted Lift Power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMecanum.java
@@ -163,7 +163,7 @@ public class TeleOpMecanum extends LinearOpMode {
             double liftPosition = ENCODER_CPR * ROTATE* gamepad2.left_trigger*.71;
             motorLift.setTargetPosition((int) liftPosition);
             motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            motorLift.setPower(.5);
+            motorLift.setPower(.2);
 
             if (gamepad2.right_trigger > 0.1) {
 
